d07243695a62888454fffc9189936eb6599c6266
Nik Shebeda <nikshebeda@gmail.com>
2022-11-28 19:18:50 +0200

    Updated AgoraChatMessage+Extensions

--- Sources/LiveCastChat/Model/AgoraChatMessage+Extensions.swift
+++ Sources/LiveCastChat/Model/AgoraChatMessage+Extensions.swift
@@ -6,28 +6,28 @@
 //
 
 import Foundation
-import LiveCastChat
-
-extension AgoraChatMessage {
-    func messageText() -> String? {
-        if let messageData = (self.body as? AgoraChatTextMessageBody)?.text.data(using: .utf8) {
-            let messageObject =  try? JSONDecoder().decode(ChatMessage.self, from: messageData)
-            return messageObject?.text
-        } else {
-            return nil
-        }
-    }
-    
-    func messageDate() -> String? {
-        ///FIXME: updated date for media messages
-//        let date = Date(timeIntervalSince1970: TimeInterval(localTime)/1000)
-//        return date.prettyPrinted
-        
-        if let messageData = (self.body as? AgoraChatTextMessageBody)?.text.data(using: .utf8) {
-            let messageObject =  try? JSONDecoder().decode(ChatMessage.self, from: messageData)
-            return messageObject?.sentDate.prettyPrinted
-        } else {
-            return nil
-        }
-    }
-}
+//import AgoraChat
+//
+//extension AgoraChatMessage {
+//    func messageText() -> String? {
+//        if let messageData = (self.body as? AgoraChatTextMessageBody)?.text.data(using: .utf8) {
+//            let messageObject =  try? JSONDecoder().decode(ChatMessage.self, from: messageData)
+//            return messageObject?.text
+//        } else {
+//            return nil
+//        }
+//    }
+//
+//    func messageDate() -> String? {
+//        ///FIXME: updated date for media messages
+////        let date = Date(timeIntervalSince1970: TimeInterval(localTime)/1000)
+////        return date.prettyPrinted
+//
+//        if let messageData = (self.body as? AgoraChatTextMessageBody)?.text.data(using: .utf8) {
+//            let messageObject =  try? JSONDecoder().decode(ChatMessage.self, from: messageData)
+//            return messageObject?.sentDate.prettyPrinted
+//        } else {
+//            return nil
+//        }
+//    }
+//}
