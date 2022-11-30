3f7faf65b36dcb33109c900049830948d9c194dc
Nik Shebeda <nikshebeda@gmail.com>
2022-11-30 14:54:14 +0200

    updated Chat storyboard

--- Sources/LiveCastChat/Manager/ChatControllerCreator.swift
+++ Sources/LiveCastChat/Manager/ChatControllerCreator.swift
@@ -9,6 +9,6 @@ import UIKit
 
 open class ChatControllerCreator {
     public static func chatListController() -> ChatListViewController {
-        return ChatListViewController.instantiate(from: .chat)
+        return UIStoryboard(name: "Chat", bundle: nil).instantiateViewController(withIdentifier: "ChatListViewController") as! ChatListViewController 
     }
 }
