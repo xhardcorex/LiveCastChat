2bcf6dc9ea7da006d1ff8917ccc1f2b692af2726
Nik Shebeda <nikshebeda@gmail.com>
2022-11-30 15:53:12 +0200

    updated SingleChatViewController xib

--- Sources/LiveCastChat/MessageKit/Sources/Views/Cells/TextMessageCell.swift
+++ Sources/LiveCastChat/MessageKit/Sources/Views/Cells/TextMessageCell.swift
@@ -83,7 +83,7 @@ open class TextMessageCell: MessageContentCell {
             case .text(let text), .emoji(let text):
                 let textColor = displayDelegate.textColor(for: message, at: indexPath, in: messagesCollectionView)
                 messageLabel.text = text
-                messageLabel.textColor = .red//textColor
+                messageLabel.textColor = textColor
                 if let font = messageLabel.messageLabelFont {
                     messageLabel.font = font
                 }
